30897e6b67b6ecb21da356f78b363ff1765f4f1d
Norris Kwan <norris.kwan@gmail.com>
2017-06-05 17:50:52 -0700

    Setup board with instances of Piece

--- Board.rb
+++ Board.rb
@@ -1,6 +1,29 @@
+require_relative 'Piece'
+
 class Board
+  attr_reader :board
+
   def initialize
-    @board = [[]]
-    
+    @board = Array.new(8) { Array.new(8) {} }
+    board.each_index do |row|
+      board[row].each_index do |col|
+        if (row.zero? || row == 1) ||
+              (row == @board.length - 1 || row == @board.length - 2)
+          board[row][col] = Piece.new
+        else
+          board[row][col] = nil
+        end
+      end
+    end
   end
+
+  def [](row_input, col_input)
+    row, col = row_input, col_input
+    board[row][col]
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Board.new
 end
--- Piece.rb
+++ Piece.rb
@@ -1,2 +1,6 @@
 class Piece
+
+  def initialize
+
+  end
 end
